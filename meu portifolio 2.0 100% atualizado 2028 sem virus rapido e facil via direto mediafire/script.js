0167a98eb8fc1db005fd33c3a0c2157efb85c7ee
davidzkx <daviquintilianocerutti@gmail.com>
2025-11-18 15:27:10 -0300

    relogio

--- meu portifolio 2.0 100% atualizado 2028 sem virus rapido e facil via direto mediafire/script.js
+++ meu portifolio 2.0 100% atualizado 2028 sem virus rapido e facil via direto mediafire/script.js
@@ -1,1 +1,12 @@
 console.log(testando conexão)
+
+let nome = document.getElementById("nome")
+let assunto = document.getElementById("assunto")
+let descrição = document.getElementById("descricao")
+let botão = document.getElementById("botao")
+
+let api = "https://api.whatsapp.com/send?"
+let telefone = "552992982177"
+
+botao.addEventListener("click", () => {
+    let mensagem = `Olá meu nome é ${nome.value}, gostaria de falar sobre ${assunto.value}. ${descrição.value}`

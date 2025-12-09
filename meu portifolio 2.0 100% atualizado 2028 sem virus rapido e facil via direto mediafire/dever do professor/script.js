029c10cc42c60cf25ac5d1f7014d3c91b9d6f505
davidzkx <daviquintilianocerutti@gmail.com>
2025-12-09 15:27:43 -0300

    gean

--- meu portifolio 2.0 100% atualizado 2028 sem virus rapido e facil via direto mediafire/dever do professor/script.js
+++ meu portifolio 2.0 100% atualizado 2028 sem virus rapido e facil via direto mediafire/dever do professor/script.js
@@ -1,8 +1,32 @@
 
-let botao = document.getElementById("botao")
-let biografia = document.getElementById("biografia")
+let botao = document.getElementById("botao");
+let biografia = document.getElementById("biografia");
 
 botao.addEventListener("click", () => {
-    biografia.classList.toggle("esconder")
-})
+    biografia.classList.toggle("esconder");
+});
 
+
+let form = document.getElementById("form");
+let tarefaInput = document.getElementById("tarefa");
+let lista = document.getElementById("lista");
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    
+    let textoTarefa = tarefaInput.value.trim();
+    
+    if (textoTarefa !== "") {
+        let novoItem = document.createElement("li");
+        novoItem.textContent = textoTarefa;
+        
+        
+        novoItem.addEventListener("click", () => {
+            novoItem.classList.toggle("concluído");
+        });
+        
+        lista.appendChild(novoItem);
+        
+        tarefaInput.value = "";
+    }
+});
